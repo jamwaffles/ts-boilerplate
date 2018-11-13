27cadbc5a806aa44effcafdbbaf28ae0ad70602e
James Waples <jamwaffles@gmail.com>
2018-11-13 20:11:50 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+import server from './server';
+
+const port = process.env.PORT || 7175;
+
+server.listen(port);
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Thing = () => (
+	<div>I'm not a toaster</div>
+);
+
+export default Thing;
