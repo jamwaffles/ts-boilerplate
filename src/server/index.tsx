5fc7ed308663c8f656d31fefaf7b8439cd6caea7
James Waples <james@wapl.es>
2019-01-18 14:04:39 +0000

    Add Winston logging

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -5,6 +5,7 @@ import * as assets from 'koa-static-cache';
 import * as webpack from 'webpack';
 import * as devMiddleware from 'webpack-dev-middleware';
 import * as koaWebpack from 'koa-webpack';
+import logger from './logger';
 import * as Webpack from 'webpack';
 import { StaticRouter } from 'react-router';
 
@@ -45,6 +46,8 @@ koaWebpack({
   });
 
   const port = process.env.PORT || 7175;
+
+  logger.info('serverStarted', { port });
 
   app.listen(port);
 });
